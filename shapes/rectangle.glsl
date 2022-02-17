e6a341a7875622bc89b6068d74608efca5089612
Elise Vidal <evidal@artfx.fr>
2022-02-17 15:53:48 +0100

    WIP Mondrian example

--- shapes/rectangle.glsl
+++ shapes/rectangle.glsl
@@ -41,8 +41,47 @@ void main(){
     //color = vec3(rectangle(st, vec2(0.2,0.5), vec2(0.2,0.2)));
 
     //Colored rectangles
-    color = vec3(rectangle(st, vec2(0.2,0.5), vec2(0.2,0.2)), rectangle(st, vec2(0.2,0.5), vec2(0.35,0.25)), 
-    rectangle(st, vec2(0.2,0.5), vec2(0.5,0.3)));
+    //color = vec3(rectangle(st, vec2(0.2,0.5), vec2(0.2,0.2)), rectangle(st, vec2(0.2,0.5), vec2(0.35,0.25)), 
+    //rectangle(st, vec2(0.2,0.5), vec2(0.5,0.3)));
+
+    //Mondrian because you know... rectangles
+
+    //Define colors
+    vec3 white = vec3(0.9137, 0.8824, 0.7373);
+    vec3 red = vec3(0.5255, 0.051, 0.051);
+    vec3 blue = vec3(0.1451, 0.349, 0.7882);
+
+    //Block positions and sizes
+    // bl_* = black
+    // b_* = blue
+    // r_* = red
+    // w_* = white
+
+    vec2 bl1_size = vec2(1.,0.1);
+    vec2 bl1_pos = vec2(0.,0.);
+
+    vec2 b1_size = vec2(0.4,0.3);
+    vec2 b1_pos = vec2(0.,bl1_size.y);
+
+    vec2 r1_size = vec2(0.3, b1_size.y);
+    vec2 r1_pos = vec2(b1_size.x, bl1_size.y);
+
+    vec2 bl2_size = vec2(.1,1.);
+    vec2 bl2_pos = vec2(r1_size.x+b1_size.x,0.);
+
+    vec2 w1_size = vec2(0.2,b1_size.y);
+    vec2 w1_pos = vec2(r1_size.x+b1_size.x+bl2_size.x, bl1_size.y);
+
+    vec2 w2_size = vec2(b1_size.x+r1_size.x, 0.2);
+    vec2 w2_pos = vec2(0., bl1_size.y+b1_size.y);
+
+    color = vec3(rectangle(st, bl1_size, bl1_pos));
+    color += vec3(rectangle(st, b1_size, b1_pos))*blue;
+    color += vec3(rectangle(st, r1_size, r1_pos))*red;
+    color += vec3(rectangle(st, bl2_size, bl2_pos));
+    color += vec3(rectangle(st, w1_size, w1_pos))*white;
+    color += vec3(rectangle(st, w2_size, w2_pos))*white;
+
 
 	gl_FragColor = vec4(color, 1.0 );
 }
