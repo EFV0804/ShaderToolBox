478819dabebc975a44c24f7c056e75db09f98625
EFV0804 <evidal@artfx.fr>
2022-10-14 10:44:15 +0200

    2d transform demo frag shader

--- movement/2d_matrices.glsl
+++ movement/2d_matrices.glsl
@@ -2,43 +2,23 @@
 precision mediump float;
 #endif
 
-
-#define PI 3.14159265359
-
 uniform vec2 u_resolution;
 uniform float u_time;
-float rectangle(vec2 st, vec2 size, vec2 pos){
 
-    // A rectangle is formed by making 4 "strips" of "non-rectangle" around the desired rectangle.
-    // We are deducting from the user's input what isn't a rectangle, and coloring that black
-    
-    //We start by using the size input to determine the top height and the left width. The rect's
-    //point of origin is therefore it's top left corner.
-    float top_height = pos.y;
-    float left_width = pos.x;
+float circle(in vec2 _st, float radius, vec2 centre){
 
-    // Once the first "strips" are set, we need to deduct what size the other 2 "strips" should be.
-    // First the position of the rectangle is substracted from the normalised max coordinate, this
-    // gives us the remaining space, and matches all strips, to begin where the others end.
-    // we substract the size of the rect, to know how much room to leave "open"
-
-    float bottom_height = ((1.-pos.y)-size.y);
-    float right_width = ((1.-pos.x)-size.x);
-
-    //Bottom Left
-    vec2 bl = step(vec2(left_width, bottom_height),st);
-    float color = bl.x * bl.y;
-
-    //Top Right
-    vec2 tr = step(vec2(right_width, top_height),1.0-st);
-    color *= tr.x * tr.y;
-
-    return color;
+    vec2 dist = _st-centre;
+	return (1.-smoothstep(radius,radius, dot(dist,dist)));
 }
 
-mat2 rotate2d(float _angle){
+float circle_outline(in vec2 _st, float radius, vec2 centre){
 
-    return mat2(cos(_angle), -sin(_angle), sin(_angle), cos(_angle));
+    float funky_radius = radius;
+    float dist = length(_st-centre);
+
+	float outline = (1.-smoothstep(funky_radius,funky_radius, dist-0.005/2.)) - (1.-smoothstep(funky_radius,funky_radius, dist+0.005/2.));
+
+    return outline;
 }
 
 mat2 scale(vec2 _scale){
@@ -46,29 +26,62 @@ mat2 scale(vec2 _scale){
                 0.0,_scale.y);
 }
 
+mat2 rotate2d(float _angle){
+
+    return mat2(cos(_angle), -sin(_angle), sin(_angle), cos(_angle));
+}
+
+float rectangle(vec2 st, vec2 size, vec2 pos){
+
+    float top_height = pos.y;
+    float left_width = pos.x;
+    float bottom_height = ((1.-pos.y)-size.y);
+    float right_width = ((1.-pos.x)-size.x);
+
+    //Bottom Left
+    vec2 bl = step(vec2(left_width, bottom_height),st);
+    float color = bl.x * bl.y;
+    //Top Right
+    vec2 tr = step(vec2(right_width, top_height),1.0-st);
+    color *= tr.x * tr.y;
+
+    return color;
+}
+
 void main(){
+    // OVERALL COORDINATES
     vec2 st = gl_FragCoord.xy/u_resolution.xy;
-    // vec2 c = u_resolution/2.0;
+    // CENTER COORDINATES
+    vec2 c = gl_FragCoord.xy/u_resolution.xy-.5;
+
     vec3 color = vec3(0.0);
 
-    //color -= vec3(rectangle(st, vec2(.5,.5), vec2(.25,.25)));
-    //move st to center of coord system
-    st -= vec2(0.5);
-    //rotate
-    st = rotate2d(-u_time*0.5)*st;
-    //scale
-    float scalar = 20.;
-    // float scalar = sin(st.x+2.); // curves outer edge of square
-    st = scale(vec2(scalar))*st;
+    // SHAPES
+    vec2 circle1_st = c;
+    circle1_st = circle1_st * rotate2d(-u_time*0.5);
+    color += circle(circle1_st, 0.002, vec2(0.1));
 
-    //move st back to original position
+    vec2 circle2_st = c;
+    circle2_st= circle2_st * rotate2d(u_time * 0.2);
+    color += circle_outline(circle2_st, 0.02, vec2(0.3))*vec3(1.,0.,0.);
+
+    vec2 circle3_st = c;
+    circle3_st= circle3_st * rotate2d(-u_time * 0.2);
+    color += circle(circle3_st, sin(0.002+u_time)/44., vec2(0.2))*vec3(0.,0.,1.);
+
+    vec2 square_st = c;
+    square_st = square_st * rotate2d(u_time);
+    square_st += vec2(0.5);
+    color += rectangle(square_st, vec2(0.25), vec2(0.39)) * vec3(0.,1.,0.);
+
+
+    // ROTATE BACKGROUND (st, overall coordinates)
+    st -= vec2(0.5);
+    st = st * rotate2d(-u_time);
     st += vec2(0.5);
 
-    color += vec3(rectangle(st, vec2(.5,.5), vec2(.25,.25)));
+    // color += vec3(st.x, st.y, 0.); // <-- display st as color
 
-    // color += vec3(rectangle(st, vec2(.5,.5), vec2(.01,.01)));
 
-    // color += vec3(st.x, st.y, 0.);
-   
     gl_FragColor = vec4(color,1.0);
 }
