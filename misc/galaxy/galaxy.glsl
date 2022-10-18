e9abb6989d47535224f6932b5a375ddea3678e76
EFV0804 <evidal@artfx.fr>
2022-10-18 17:09:07 +0200

    added volume noise layers + color tweak + star tweak

--- misc/galaxy/galaxy.glsl
+++ misc/galaxy/galaxy.glsl
@@ -20,17 +20,20 @@ vec2 random2( vec2 p )
 {
     return fract(sin(vec2(dot(p,vec2(127.1,311.7)),dot(p,vec2(269.5,183.3))))*43758.5453);
 }
-float star(in vec2 _st, in float flare_mult){
+vec2 star(in vec2 _st, in float flare_mult){
 
     float d = length(_st);
-    float m = 0.13/d;
+    vec2 m;
+    m.x = 0.13/d;
     float flare = max(0.,1.-abs(_st.x*_st.y*1000.));
     mat2 rot = mat2(cos(3.14/4.), -sin(3.14/4.), sin(3.14/4.), cos(3.14/4.));
     _st *= rot;
     flare += max(0.,1.-abs(_st.x*_st.y*1000.));
-    m += flare * flare_mult;
-    m *= smoothstep(0.5,0.2,d);
+    m.y = flare * flare_mult;
+    m.x += smoothstep(0.5,0.3,d);
+    // m *= smoothstep(0.5,0.3,d;
     return m;
+
 }
 vec3 star_layer(in vec2 st){
     float m;
@@ -48,8 +51,9 @@ vec3 star_layer(in vec2 st){
             // point = 0.5*sin(u_time + 6.2831*point);
             vec2 diff = neighbor + point - f_st;
             size = fract(Hash21(i_st+neighbor));
-            float m = star(diff, smoothstep(0.8, 1., size));
-            color += m*(vec3(.3*size,0.3*size,.5*size))*size;
+            vec2 m = star(diff, 0.1*size);
+            color += m.x*vec3(.5*(size*0.4),0.5*(size*0.2),.5*(size*0.2));
+            color += m.y*vec3(0.9333, 1.0, 0.0);
         }
     }
     
@@ -108,12 +112,92 @@ float fbm_warp(in vec2 st, in mat2 rot) {
     }
     return value;
 }
+vec2 fade(vec2 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
+vec4 permute(vec4 i) {
+vec4 im = mod(i, 289.0);
+return mod(((im*34.0)+10.0)*im, 289.0);
+}
+float cnoise(vec2 P){
+  vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+  vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+  Pi = mod(Pi, 289.0); // To avoid truncation effects in permutation
+  vec4 ix = Pi.xzxz;
+  vec4 iy = Pi.yyww;
+  vec4 fx = Pf.xzxz;
+  vec4 fy = Pf.yyww;
+  vec4 i = permute(permute(ix) + iy);
+  vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; // 1/41 = 0.024...
+  vec4 gy = abs(gx) - 0.5;
+  vec4 tx = floor(gx + 0.5);
+  gx = gx - tx;
+  vec2 g00 = vec2(gx.x,gy.x);
+  vec2 g10 = vec2(gx.y,gy.y);
+  vec2 g01 = vec2(gx.z,gy.z);
+  vec2 g11 = vec2(gx.w,gy.w);
+  vec4 norm = 1.79284291400159 - 0.85373472095314 * 
+    vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
+  g00 *= norm.x;
+  g01 *= norm.y;
+  g10 *= norm.z;
+  g11 *= norm.w;
+  float n00 = dot(g00, vec2(fx.x, fy.x));
+  float n10 = dot(g10, vec2(fx.y, fy.y));
+  float n01 = dot(g01, vec2(fx.z, fy.z));
+  float n11 = dot(g11, vec2(fx.w, fy.w));
+  vec2 fade_xy = fade(Pf.xy);
+  vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+  float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+  return 2.3 * n_xy;
+}
+float psrdnoise(vec2 x, vec2 period, float alpha, out vec2 gradient)
+{
+	vec2 uv = vec2(x.x+x.y*0.5, x.y);
+	vec2 i0 = floor(uv), f0 = fract(uv);
+	float cmp = step(f0.y, f0.x);
+	vec2 o1 = vec2(cmp, 1.0-cmp);
+	vec2 i1 = i0 + o1, i2 = i0 + 1.0;
+	vec2 v0 = vec2(i0.x - i0.y*0.5, i0.y);
+	vec2 v1 = vec2(v0.x + o1.x - o1.y*0.5, v0.y + o1.y);
+	vec2 v2 = vec2(v0.x + 0.5, v0.y + 1.0);
+	vec2 x0 = x - v0, x1 = x - v1, x2 = x - v2;
+	vec3 iu, iv, xw, yw;
+	if(any(greaterThan(period, vec2(0.0)))) {
+		xw = vec3(v0.x, v1.x, v2.x);
+		yw = vec3(v0.y, v1.y, v2.y);
+		if(period.x > 0.0)
+			xw = mod(vec3(v0.x, v1.x, v2.x), period.x);
+		if(period.y > 0.0)
+			yw = mod(vec3(v0.y, v1.y, v2.y), period.y);
+		iu = floor(xw + 0.5*yw + 0.5); iv = floor(yw + 0.5);
+	} else {
+		iu = vec3(i0.x, i1.x, i2.x); iv = vec3(i0.y, i1.y, i2.y);
+	}
+	vec3 hash = mod(iu, 289.0);
+	hash = mod((hash*51.0 + 2.0)*hash + iv, 289.0);
+	hash = mod((hash*34.0 + 10.0)*hash, 289.0);
+	vec3 psi = hash*0.07482 + alpha;
+	vec3 gx = cos(psi); vec3 gy = sin(psi);
+	vec2 g0 = vec2(gx.x, gy.x);
+	vec2 g1 = vec2(gx.y, gy.y);
+	vec2 g2 = vec2(gx.z, gy.z);
+	vec3 w = 0.8 - vec3(dot(x0, x0), dot(x1, x1), dot(x2, x2));
+	w = max(w, 0.0); vec3 w2 = w*w; vec3 w4 = w2*w2;
+	vec3 gdotx = vec3(dot(g0, x0), dot(g1, x1), dot(g2, x2));
+	float n = dot(w4, gdotx);
+	vec3 w3 = w2*w; vec3 dw = -8.0*w3*gdotx;
+	vec2 dn0 = w4.x*g0 + dw.x*x0;
+	vec2 dn1 = w4.y*g1 + dw.y*x1;
+	vec2 dn2 = w4.z*g2 + dw.z*x2;
+	gradient = 10.9*(dn0 + dn1 + dn2);
+	return 10.9*n;
+}
+
 void main(){
 
     vec2 st = (gl_FragCoord.xy-0.5*u_resolution.xy)/u_resolution.y;
         vec2 c = gl_FragCoord.xy/u_resolution.xy-.5;
 
-    float scalar = 10.;
+    float scalar = 3.;
     st *= scalar;
 
     vec2 i_st = floor(st);
@@ -134,24 +218,69 @@ void main(){
 
 // ----------------------------------------------------
 
-    float a = 0.5;
+    float q, b, d, e, f, g, h, i;
+    // a = modifier for fbm_warp rotation and st scale on cnoise
+    //TODO better random
+    float a = random(vec2(1.4845))+0.5;
+    // float a = 0.5;
     mat2 r = mat2(cos(a), -sin(a), sin(a), cos(a));
+    float v;
 
-    float v = fbm_warp(st, r);
-    v = smoothstep(0.9, 0.2, v);
-    color += v*vec3(0.5608, 0.5529, 0.549);
 
-    v = fbm_warp(st*.231, r);
+
+    // red volumes
+
+    q = +cnoise(st*(a*4.1)+cnoise(st*(a*2.)));
+    b = cnoise(st*(a*.1)+cnoise(st*(a*0.6)));
+    //billowing: abs of 2 perlins
+    d = abs(q-b);
+
+    e = cnoise(st*(a*0.2)+cnoise(st*(a*0.6)));
+    f = cnoise(st*(a*0.21)+cnoise(st*(a*.5)));
+    g = abs(e+f);
+
+    h = (d*g);
+    h = fbm(st)*(h);
+
+    v = fbm_warp(st*(a*0.5), r);
     v = smoothstep(0.58, 1.1, v);
-    color += v*vec3(0.4275, 0.1333, 0.0);
-    
-    v = fbm_warp(st*0.5, r);
-    v = smoothstep(0.7, 0., v);
-    color += v*vec3(0.4941, 0.298, 0.0078);
 
-    v = fbm_warp(st*1.6, r);
-    v = smoothstep(0.6, 1.9, v);
-    color += v*vec3(0.0314, 0.2941, 0.4745);
+    color += v*(h*0.8)*vec3(0.5804, 0.3255, 0.2275);
+    color += v*vec3(0.4667, 0.1765, 0.0118)*0.2;
+
+    // ------------------------------------------
+
+    // space backdrop
+
+    float space_backdrop = fbm_warp(st*(a*1.9), r);
+    space_backdrop = smoothstep(0.6, 1.6, space_backdrop);
+    color += space_backdrop*vec3(0.0431, 0.2078, 0.3216);
+
+    // yellow volumes
+    v = fbm_warp(st*(a*1.3), r);
+    v = smoothstep(0.88, 0.2, v);
+ 
+    color += v*vec3(0.4941, 0.298, 0.0078)*(space_backdrop*0.9);
+
+
+    q = +cnoise(st*(a*.1)+cnoise(st*(a*1.8)));
+    b = cnoise(st*(a*.128)+cnoise(st*(a*.6)));
+        //billowing: abs of 2 perlins
+    d = abs(q-b);
+
+    e = cnoise(st*(a*0.3)+cnoise(st*(a*.2)));
+    f = cnoise(st*(a*.4)+cnoise(st*(a*.214)));
+    g = abs(e+f);
+    h = (d*g);
+    h = fbm(st)*(h*a);
+
+    color += (h*3.5)*v*vec3(0.3922, 0.3882, 0.1294);
+    color += v*vec3(0.4941, 0.298, 0.0078)*0.7;
+
+    // overall illumination of the scene
+    float lightness = cnoise(st*(a*0.3)+fbm(st+0.4));
+    color += (lightness*vec3(0.9137, 0.898, 0.898))*0.15;
+
 
 
     gl_FragColor = vec4(color,1.0);
